9435c2029b0bd0ca9872fd45f8c54d49d99567ac
amanueln <namanuel@castlemedical.com>
2018-01-12 01:12:53 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -135,12 +135,17 @@ function initMap() {
                 map.setZoom(12);
             });
             //if filter textarea is clicked infowindow closes 
-            document.getElementById("input").addEventListener("click", function(){ 
-            if (infowindow != null) { 
+            var inputClick = document.getElementById("input");
+            if (inputClick){
+                inputClick.addEventListener("click", function(){ 
+                 if (infowindow != null) { 
                 infowindow.close(); 
                 map.setCenter(infowindow.marker.getPosition());
                   } 
             });
+                
+            }
+
                 }
     }
     //opens infowindow when list is clicked.
